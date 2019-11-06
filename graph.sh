9f190f39989a702ebfe1b309b2a311a6b975f715
Votre Nom <Vous@exemple.com>
2019-11-06 22:56:10 +0100

    first

---
+++ graph.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+file=${2:?"Vous devez fournir le nom du fichier .dot en argument."}
+
+
---
+++ traceroute.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+		# Test si argument (adresse) présent
+presAddr=${1:?"Vous devez fournir l'adresse à atteindre en argument."}
+		# Déclaration de toutes les méthodes à tester (protocole + port)
+declare -a meth=("-I" "-U -p 53" "-T -p 443" "-T -p 22" "-T -p 25" "-T -p 80");
+compteur=1;
+# Début de la boucle traceroute
+for compteur in $(seq 1 30); do
+	
+	for i  in "${meth[@]}"; do
+
+		echo -e "Lancement de la commande suivante : traceroute -A -n -w2 -q1 $i -f $compteur -m $compteur $presAddr"
+		# Commande traceroute utilisée 
+		tr=$(sudo traceroute -A -n $i -w2 -q1 -f $compteur -m $compteur $presAddr |sed 's/*/#/g' | sed -n "2p")
+
+		# Test si le routeur ne répond pas (affichage d'étoiles)
+		if ! [[ $(echo $tr |awk '{print $2}') = "#" ]]; then 
+			as=$(echo $tr |awk '{print $3}')	# Récupération du numéro d'AS
+			addr=$(echo $tr|awk '{print $2}')	# Récupération de l'adresse IP du routeur au saut actuel
+			echo -e "Le routeur répond à la méthode suivante: $i \n $tr"
+			break
+		elif [[ $i = "-T -p 80" ]]; then
+			as="#"
+			addr="$compteur"
+			echo "Le routeur n°$compteur ne répond pas"
+		else
+			echo "Le routeur n°$compteur ne répond pas à la méthode $i"		
+		fi
+	done
+	compteur=$(($compteur+1))
+	echo $as $addr >> $presAddr.txt	
+	if [[ $presAddr == $addr ]]; then
+		break
+	fi
+	
+done
+
+# Enregistrement pour le graph
+echo "L'utilitaire Traceroute a atteind avec succès le routeur final 
+Voulez-vous donc créer le graphique Xdot ? [y-n]"
+read $quest 
+if [[ $(echo $quest) = "y" ]]; then 
+	echo $(sudo ./graph.sh $presAddr.txt)
+	echo "Le graphique Xdot a été créé"	
+fi
